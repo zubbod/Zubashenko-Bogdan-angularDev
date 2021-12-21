2321c7cc5b14f5325c01e18487ef973cc2e78051
Zubashenko Bogdan <bz@agentapp.ru>
2021-12-21 21:48:57 +0300

    fix errors interceptor

--- src/app/core/interceptors/errors.interceptor.ts
+++ src/app/core/interceptors/errors.interceptor.ts
@@ -19,39 +19,39 @@ export class ErrorsInterceptor implements HttpInterceptor {
     request: HttpRequest<unknown>,
     next: HttpHandler,
   ): Observable<HttpEvent<unknown>> {
-    return next.handle(request).pipe(catchError(this.handleError));
-  }
-
-  private handleError(error: HttpErrorResponse): Observable<never> {
-    switch (error.status) {
-      case HttpStatusCode.BadRequest:
-        this.messageService.showWarning(
-          'Request had bad syntax or the parameters supplied were invalid.',
-        );
-        break;
-      case HttpStatusCode.Unauthorized:
-        this.messageService.showWarning('Unauthorized. API authorization failed.');
-        break;
-      case HttpStatusCode.Forbidden:
-        this.messageService.showWarning(
-          'Unauthorized. You do not have permission to access this endpoint.',
-        );
-        break;
-      case HttpStatusCode.NotFound:
-        this.messageService.showWarning('Server has not found a route matching the given URI.');
-        break;
-      case HttpStatusCode.InternalServerError:
-        this.messageService.showWarning(
-          'Server encountered an unexpected condition which prevented it from fulfilling the request.',
-        );
-        break;
-      case HttpStatusCode.ServiceUnavailable:
-        this.messageService.showWarning('Server unavailable.');
-        break;
-      default:
-        this.messageService.showWarning(`${error.statusText} ${error.message}`);
-        break;
-    }
-    return throwError(error);
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        switch (error.status) {
+          case HttpStatusCode.BadRequest:
+            this.messageService.showWarning(
+              'Request had bad syntax or the parameters supplied were invalid.',
+            );
+            break;
+          case HttpStatusCode.Unauthorized:
+            this.messageService.showWarning('Unauthorized. API authorization failed.');
+            break;
+          case HttpStatusCode.Forbidden:
+            this.messageService.showWarning(
+              'Unauthorized. You do not have permission to access this endpoint.',
+            );
+            break;
+          case HttpStatusCode.NotFound:
+            this.messageService.showWarning('Server has not found a route matching the given URI.');
+            break;
+          case HttpStatusCode.InternalServerError:
+            this.messageService.showWarning(
+              'Server encountered an unexpected condition which prevented it from fulfilling the request.',
+            );
+            break;
+          case HttpStatusCode.ServiceUnavailable:
+            this.messageService.showWarning('Server unavailable.');
+            break;
+          default:
+            this.messageService.showWarning(`${error.statusText} ${error.message}`);
+            break;
+        }
+        return throwError(error);
+      }),
+    );
   }
 }
